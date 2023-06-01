22527f3c6bd46ffdf0739f1634db8d377d9b44b3
Alice Marcaillou <alice.marcaillou8@gmail.com>
2023-06-01 14:40:54 +0200

    Fomulaire des contact Fait modification de index.twig de contact et son controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\ContxtFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +13,14 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="app_contact")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $form=$this->createForm(ContxtFormType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid()){
+            dump($form->getData());
+        }
+
         return $this->render('contact/index.html.twig', [
             'form'=>$form->createView(),
         ]);
