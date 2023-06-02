3eab00c8e1ef73dbeaff7987207ac75d008b1ff2
Alice Marcaillou <alice.marcaillou8@gmail.com>
2023-06-02 16:50:16 +0200

    ajouter un utilisateur a une tache

--- src/Form/TodoType.php
+++ src/Form/TodoType.php
@@ -20,6 +20,9 @@ class TodoType extends AbstractType
             ->add('priority', EntityType::class,[
                 'class'=>'App\Entity\Priority',
                  'choice_label'=>'name'])
+            ->add('user', EntityType::class,[
+                'class'=>'App\Entity\User',
+                 'choice_label'=>'name'])
         ;
     }
 
